00b75c9bc680c50a62b5355b4a018354c2f60946
Anjiang-Wei <1020681930@qq.com>
2022-02-13 07:39:04 +0000

    return to normal

--- src/theory/arith/idl/idl_extension.cpp
+++ src/theory/arith/idl/idl_extension.cpp
@@ -456,7 +456,7 @@ std::vector<TNode> IdlExtension::spfa_early_terminate(size_t node1)
 			{
 				pre[v] = u;
 				dis[v] = dis[u] + w;
-				if (++iter == 32)
+				if (++iter == n_spfa)
         {
             iter = 0;
             result = detect_cycle();
